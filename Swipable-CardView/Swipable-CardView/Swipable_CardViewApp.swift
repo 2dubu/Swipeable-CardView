3c2bd85aa02bacf44a84a0cd306bedf45a901ba5
2dubu <2dubu.dev@gmail.com>
2023-03-28 00:19:18 +0900

    :tada: :: tada

---
+++ Swipable-CardView/Swipable-CardView/Swipable_CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipable_CardViewApp.swift
+//  Swipable-CardView
+//
+//  Created by 이건우 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct Swipable_CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
